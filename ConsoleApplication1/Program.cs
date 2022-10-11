281ae07586cc59049b1f52404fc7499db092ec9c
Taha Amin Ghafuri <tahaghafuri@gmail.com>
2022-10-11 19:51:25 +0330

    first commit

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Enter To Echo:");
+            Console.ResetColor();
+            string str = Convert.ToString(Console.ReadLine());
+            Console.WriteLine(str);
+            Console.ReadKey();
+        }
+    }
+}
